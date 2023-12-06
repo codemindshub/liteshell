ccd5a1d1008b9d233f7ba06e1f6f743d1a7f5c50
tijesus <emmanueltijesu@gmail.com>
2023-12-06 18:11:19 +0100

    Function handling environmantal variables

---
+++ exec_env.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+/**
+ * main - function to execute environment variables
+*/
+
+int main(int argc, char **argv, char **environ) {
+    char **env = environ;
+
+    if (argc > 1 && strcmp(argv[1], "invoke_env") == 0) {
+		/* Print environment variables */
+		while (*env != NULL) {
+			printf("%s\n", *env);
+			env++;
+        }
+    } else {
+        /* Construct the command without using system, avoiding shell injection */
+        char *command[] = {"sh", "-c", "env", NULL};
+
+        // Create a child process
+        pid_t pid = fork();
+
+        if (pid == 0) {
+			/* This is the child process */
+		    execve("/bin/sh", command, environ);
+			/* execve only returns if an error occurs */            perror("execve");
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+			/* Fork failed */
+		    perror("fork");
+            exit(EXIT_FAILURE);
+        } else {
+		/* This is the parent process */
+		/* Wait for the child to complete */
+            waitpid(pid, NULL, 0);
+        }
+    }
+
+    return 0;
+}
